5d07ad8f19333b6f07912886f39ad67b12f00ff8
이준영 <junlight94@gmail.com>
2025-04-23 08:34:56 +0900

    feat. 1단계 (로또 최고 순위와 최저 순위)

--- Programmers/Programmers/main.swift
+++ Programmers/Programmers/main.swift
@@ -7,7 +7,10 @@
 
 import Foundation
 
-print(mainSolution())
-func mainSolution() -> String {
-    return ""
+print(mainSolution([44, 1, 0, 0, 31, 25], [31, 10, 45, 1, 6, 19]))
+func mainSolution(_ lottos: [Int], _ win_nums: [Int]) -> [Int] {
+    let zeroCount = lottos.filter { $0 == 0 }.count
+    let winCount = win_nums.filter { lottos.contains($0) }.count
+    
+    return [min(7 - winCount - zeroCount, 6), min(7 - winCount, 6)]
 }
